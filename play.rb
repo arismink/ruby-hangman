e7ff0e712f81f64732ee2d51df8f6fb7cb8d6519
arismink <krismina.la@granify.com>
2022-08-13 21:24:16 -0600

    create array of sample words

---
+++ play.rb
@@ -0,0 +1,19 @@
+class Hangman
+    def initialize      
+        # convert to an array
+        # once you make a guess, we want to remove that letter from the available letters to guess from
+        letters =('a'..'z').to_a
+        word = words.sample
+    end
+
+    def words
+        [
+            ['cricket', 'a game play by people']
+        ]
+    end
+    
+
+    def begin
+    
+    end
+end

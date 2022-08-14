647138db7aba39019c9090deb38e071e27c2a835
arismink <krismina.la@granify.com>
2022-08-13 21:46:58 -0600

    add more words

--- play.rb
+++ play.rb
@@ -1,6 +1,6 @@
-class Hangman
-    def initialize      
-        # convert to an array
+def initialize      
+    class Hangman
+    # convert to an array
         # once you make a guess, we want to remove that letter from the available letters to guess from
         letters =('a'..'z').to_a
         word = words.sample
@@ -8,12 +8,17 @@ class Hangman
 
     def words
         [
-            ['cricket', 'a game play by people']
+               ['cricket', 'a game play by gentlemen'],
+               ['jogging', 'we are not walking...'],
+               ['celebrate', 'a game play by gentlemen'],
+               ['continent', 'there are 7 of these'],
+               ['exotic', 'not from around here...'],
+            
         ]
     end
     
-
     def begin
+    
     
     end
 end
